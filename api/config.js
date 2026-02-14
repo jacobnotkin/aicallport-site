8bbb66d8c4b742a35412608f1ba6a6317e0f4c7d
jacobnotkin <jacobhomeplex@gmail.com>
2026-02-14 12:54:59 -0800

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,11 @@
 export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
+
+  const vapiPublicKey = process.env.VAPI_PUBLIC_KEY || "";
+  const assistantId = process.env.VAPI_ASSISTANT_ID || "";
+
   res.status(200).json({
-    vapiPublicKey: process.env.VAPI_PUBLIC_KEY || ""
+    vapiPublicKey,
+    assistantId
   });
 }
-
